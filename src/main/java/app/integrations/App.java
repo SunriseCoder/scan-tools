d79430774c6c916bbcfa69de0e7c18770db421b6
SunriseCoder <sunrisecoder@gmail.com>
2017-09-12 12:47:53 +0300

    Created maven project

---
+++ src/main/java/app/integrations/App.java
@@ -0,0 +1,13 @@
+package app.integrations;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
