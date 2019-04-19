f69a20c11d5558d61df6c79e02f5cba2098f86d6
SunriseCoder <sunrisecoder@gmail.com>
2019-04-20 02:33:33 +0300

    page-crop - Added OpenFolder dialog to ImageViewer

--- page-crop/src/main/java/crop/ImageViewerApp.java
+++ page-crop/src/main/java/crop/ImageViewerApp.java
@@ -4,22 +4,22 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class ImageViewerApp extends Application {
-	private static String filename;
+    private static String filename;
 
-	public static void main(String[] args) {
-		if (args.length < 1) {
-			System.out.println("Use: " + ImageViewerApp.class.getName() + " <filename>");
-			System.exit(-1);
-		}
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Use: " + ImageViewerApp.class.getName() + " <filename>");
+            System.exit(-1);
+        }
 
-		filename = args[0];
+        filename = args[0];
 
-    	launch(args);
+        launch(args);
     }
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		ImageViewer imageViewer = new ImageViewer();
-		imageViewer.start(primaryStage, filename);
-	}
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        ImageViewer imageViewer = new ImageViewer();
+        imageViewer.start(primaryStage, filename);
+    }
 }
