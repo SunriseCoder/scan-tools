fad9ea085c40e12f6894e1e56b4d54729b6840fc
SunriseCoder <sunrisecoder@gmail.com>
2019-04-20 22:26:20 +0300

    page-crop - Added file selection to ImageViewer

--- page-crop/src/main/java/crop/ImageViewerApp.java
+++ page-crop/src/main/java/crop/ImageViewerApp.java
@@ -4,22 +4,14 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class ImageViewerApp extends Application {
-    private static String filename;
 
     public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("Use: " + ImageViewerApp.class.getName() + " <filename>");
-            System.exit(-1);
-        }
-
-        filename = args[0];
-
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         ImageViewer imageViewer = new ImageViewer();
-        imageViewer.start(primaryStage, filename);
+        imageViewer.start(primaryStage);
     }
 }
