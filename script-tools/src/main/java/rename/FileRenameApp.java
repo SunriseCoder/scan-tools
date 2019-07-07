881923f2a55d8cdfb579c104d38824c2c34db827
SunriseCoder <sunrisecoder@gmail.com>
2019-07-07 13:35:04 +0300

    script-tools: FileRenameApp recursive scan

--- script-tools/src/main/java/rename/FileRenameApp.java
+++ script-tools/src/main/java/rename/FileRenameApp.java
@@ -6,9 +6,10 @@ import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.attribute.FileTime;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class FileRenameApp {
-
+    private static final Pattern FILE_PATTERN = Pattern.compile("^[0-9]{4}-[0-9]{2}-[0-9]{2}_.*$");
     private static final String CREATION_TIME_ATTRIBUTE = "creationTime";
 
     public static void main(String[] args) throws IOException {
@@ -18,27 +19,46 @@ public class FileRenameApp {
         }
 
         File folder = new File(args[0]);
-        File[] files = folder.listFiles();
 
         try (PrintWriter renameWriter = new PrintWriter("rename.bat");
                 PrintWriter undoWriter = new PrintWriter("undo-rename.bat")) {
-            for (File file : files) {
-                String oldFileName = file.getName();
-                System.out.println("Processing file: " + oldFileName);
-
-                Map<String, Object> attributes = Files.readAttributes(file.toPath(), CREATION_TIME_ATTRIBUTE);
-                FileTime creationTime = (FileTime) attributes.get(CREATION_TIME_ATTRIBUTE);
-
-                String newFileName = creationTime.toString().substring(0, 10) + "_" + oldFileName;
-                String renameCommand = "rename \"" + oldFileName + "\" \"" + newFileName + "\"";
-                renameWriter.println(renameCommand);
-
-                String undoRenameCommand = "rename \"" + newFileName + "\" \"" + oldFileName + "\"";
-                undoWriter.println(undoRenameCommand);
-            }
+            File rootFolder = folder.getAbsoluteFile().getParentFile();
+            scanFolderRecursively(rootFolder, folder.getAbsoluteFile(), renameWriter, undoWriter);
         }
 
         System.out.println("Batch files generated");
+    }
+
+    private static void scanFolderRecursively(File root, File folder, PrintWriter renameWriter, PrintWriter undoWriter)
+            throws IOException {
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                scanFolderRecursively(root, file, renameWriter, undoWriter);
+                continue;
+            }
+
+            String oldFileName = file.getName();
+            // If the File already has "YYYY-MM-DD_*" Pattern, don't rename it
+            if (FILE_PATTERN.matcher(oldFileName).matches()) {
+                continue;
+            }
+
+            String oldFileRelativeName = root.toPath().relativize(file.toPath()).toString();
+            System.out.println("Processing file: " + oldFileRelativeName);
+
+            Map<String, Object> attributes = Files.readAttributes(file.toPath(), CREATION_TIME_ATTRIBUTE);
+            FileTime creationTime = (FileTime) attributes.get(CREATION_TIME_ATTRIBUTE);
+
+            String newFileName = creationTime.toString().substring(0, 10) + "_" + oldFileName;
+            File newFile = new File(file.getParentFile(), newFileName);
+            String newFileRelativeName = root.toPath().relativize(newFile.toPath()).toString();
+            String renameCommand = "move \"" + oldFileRelativeName + "\" \"" + newFileRelativeName + "\"";
+            renameWriter.println(renameCommand);
+
+            String undoRenameCommand = "move \"" + newFileRelativeName + "\" \"" + oldFileRelativeName + "\"";
+            undoWriter.println(undoRenameCommand);
+        }
     }
 
     private static void printUsage() {
