a856f0a8a0f77eec6927b382b038cc7f2a9a6851
SunriseCoder <sunrisecoder@gmail.com>
2019-08-12 20:28:38 +0300

    book-tools: ParseForm Stubs

--- book-tools/src/main/java/process/forms/editor/ParseForm.java
+++ book-tools/src/main/java/process/forms/editor/ParseForm.java
@@ -2,13 +2,38 @@ package process.forms.editor;
 
 import java.io.IOException;
 
+import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TextArea;
 import process.context.ApplicationContext;
 import utils.FileUtils;
 
 public class ParseForm {
     private ApplicationContext applicationContext;
+
+    @FXML
+    private SplitPane parseSplitPane;
+
+    @FXML
+    private Button transformButton;
+    @FXML
+    private Button saveButton;
+    @FXML
+    private Button resetButton;
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private TextArea sourceField;
+    @FXML
+    private TextArea transformationField;
+    @FXML
+    private TextArea transformedField;
+    @FXML
+    private TextArea correctField;
 
     public Node createUI(ApplicationContext applicationContext) throws IOException {
         this.applicationContext = applicationContext;
