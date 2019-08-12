3a62966521f80a8c1799dbda485c33685fa3d8a0
SunriseCoder <sunrisecoder@gmail.com>
2019-08-12 18:47:55 +0300

    book-tools: View/Edit/Parse Tabs

--- book-tools/src/main/java/process/forms/EditorForm.java
+++ book-tools/src/main/java/process/forms/EditorForm.java
@@ -5,20 +5,39 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.Tab;
 import process.context.ApplicationContext;
+import process.forms.editor.EditForm;
+import process.forms.editor.ParseForm;
+import process.forms.editor.ViewForm;
 import utils.FileUtils;
 
 public class EditorForm {
     private ApplicationContext applicationContext;
 
     @FXML
-    private TextArea textArea;
+    private Tab viewTab;
+    @FXML
+    private Tab editTab;
+    @FXML
+    private Tab parseTab;
 
     public Node createUI(ApplicationContext applicationContext) throws IOException {
         this.applicationContext = applicationContext;
 
         Parent root = FileUtils.loadFXML(this);
+
+        ViewForm viewForm = new ViewForm();
+        Node viewFormNode = viewForm.createUI(applicationContext);
+        viewTab.setContent(viewFormNode);
+
+        EditForm editForm = new EditForm();
+        Node editFormNode = editForm.createUI(applicationContext);
+        editTab.setContent(editFormNode);
+
+        ParseForm parseForm = new ParseForm();
+        Node parseFormNode = parseForm.createUI(applicationContext);
+        parseTab.setContent(parseFormNode);
 
         return root;
     }
---
+++ book-tools/src/main/java/process/forms/editor/EditForm.java
@@ -0,0 +1,20 @@
+package process.forms.editor;
+
+import java.io.IOException;
+
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import process.context.ApplicationContext;
+import utils.FileUtils;
+
+public class EditForm {
+    private ApplicationContext applicationContext;
+
+    public Node createUI(ApplicationContext applicationContext) throws IOException {
+        this.applicationContext = applicationContext;
+
+        Parent root = FileUtils.loadFXML(this);
+
+        return root;
+    }
+}
